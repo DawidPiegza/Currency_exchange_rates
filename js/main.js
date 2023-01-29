b28468b75491d44f008e5af7863f49cf15e98574
DawidPiegza <dpiegza7@gmail.com>
2023-01-29 23:22:58 +0100

    Fixing displaying data from api

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-'strict';
+'use strict';
 
 const apiLink = 'http://api.nbp.pl/api/exchangerates/tables/a/';
 //Date Selector
@@ -24,7 +24,7 @@ const apiPromise = () =>
 		} else reject(console.log('Waiting for specyfic data...'));
 	});
 
-connectApi = () => {
+const connectApi = () => {
 	fetch(URL)
 		.then((res) => res.json())
 		.then((data) =>
@@ -37,23 +37,38 @@ connectApi = () => {
 		});
 };
 
-// const clearEverything = () => {
-// 	(newArray = []), (odpowiedz.innerText = '');
-// };
+const clearEverything = () => {
+	clearPage();
+	newArray = [];
+};
+
+const clearPage = () => {
+	const myNode = document.querySelector('.currency-container');
+	while (myNode.lastElementChild) {
+		myNode.removeChild(myNode.lastElementChild);
+	}
+};
 
 const addViewCurrency = () => {
 	for (let index = 0; index < newArray.length; index++) {
-		var p = document.createElement('p');
-		p.className = 'row';
-		var names = `WALUTA: ${newArray[index].currency} ${newArray[index].code} KURS: ${newArray[index].mid}`;
-		p.innerText = names;
-		currencyContainer.append(p);
+		let div = document.createElement('div');
+		div.className = 'currency-div';
+		let p1 = document.createElement('p');
+		p1.className = 'first-row';
+		let p2 = document.createElement('p');
+		p2.className = 'second-row';
+		let currency = `WALUTA: ${newArray[index].currency} (${newArray[index].code}) `;
+		let value = `KURS: ${newArray[index].mid}`;
+		p1.innerText = currency;
+		p2.innerText = value;
+		currencyContainer.append(div);
+		div.append(p1, p2);
 	}
 };
 
 btnConfirm.addEventListener('click', () => {
 	startDate = datePicker.value;
 	URL = apiLink + startDate + '/';
-	// clearEverything();
+	clearEverything();
 	connectApi();
 });
